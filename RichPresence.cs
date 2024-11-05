202666eafcedd157558eecc4e8728a2fdd015fd9
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-11-05 06:25:30 -0800

    Remove useless fields, comments for str len max

--- RichPresence.cs
+++ RichPresence.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using System.Text;
+﻿// ReSharper disable InconsistentNaming
 
 namespace Shizou.AnimePresence;
 
@@ -14,63 +12,42 @@ public enum ActivityType
     Competing = 5
 }
 
-[SuppressMessage("ReSharper", "InconsistentNaming")]
 public record Button
 {
-    private readonly string _label;
-    private readonly string _url;
+    /// <summary>
+    /// Max 32 bytes
+    /// </summary>
+    public required string label { get; init; }
 
-    public required string label
-    {
-        get => _label;
-        [MemberNotNull(nameof(_label))]
-        init => _label = RichPresence.GetBoundedString(value, 32);
-    }
-
-    public required string url
-    {
-        get => _url;
-        [MemberNotNull(nameof(_url))]
-        init => _url = RichPresence.GetBoundedString(value, 512);
-    }
+    /// <summary>
+    /// Max 512 bytes
+    /// </summary>
+    public required string url { get; init; }
 }
 
-[SuppressMessage("ReSharper", "InconsistentNaming")]
 public record Assets
 {
-    private readonly string? _smallImage;
-    private readonly string? _smallText;
-    private readonly string _largeImage;
-    private readonly string _largeText;
+    /// <summary>
+    /// Max 256 bytes
+    /// </summary>
+    public string? small_image { get; init; }
 
-    public string? small_image
-    {
-        get => _smallImage;
-        init => _smallImage = value is null ? null : RichPresence.GetBoundedString(value, 256);
-    }
+    /// <summary>
+    /// Max 128 bytes
+    /// </summary>
+    public string? small_text { get; init; }
 
-    public string? small_text
-    {
-        get => _smallText;
-        init => _smallText = value is null ? null : RichPresence.GetBoundedString(value, 128);
-    }
+    /// <summary>
+    /// Max 256 bytes
+    /// </summary>
+    public string? large_image { get; init; }
 
-    public required string large_image
-    {
-        get => _largeImage;
-        [MemberNotNull(nameof(_largeImage))]
-        init => _largeImage = RichPresence.GetBoundedString(value, 256);
-    }
-
-    public required string large_text
-    {
-        get => _largeText;
-        [MemberNotNull(nameof(_largeText))]
-        init => _largeText = RichPresence.GetBoundedString(value, 128);
-    }
+    /// <summary>
+    /// Max 128 bytes
+    /// </summary>
+    public string? large_text { get; init; }
 }
 
-[SuppressMessage("ReSharper", "InconsistentNaming")]
 public record TimeStamps
 {
     public long? start { get; init; }
@@ -83,58 +60,26 @@ public record TimeStamps
     };
 }
 
-[SuppressMessage("ReSharper", "InconsistentNaming")]
-public record Party
-{
-    public string id { get; init; } = "party-test";
-
-    public required int[] size { get; init; }
-}
-
-[SuppressMessage("ReSharper", "InconsistentNaming")]
 public record RichPresence
 {
-    private readonly Button[]? _buttons;
-    private readonly string _details;
-    private readonly string _state;
-
     public int type { get; init; } = (int)ActivityType.Watching;
 
-    public required string details
-    {
-        get => _details;
-        [MemberNotNull(nameof(_details))]
-        init => _details = GetBoundedString(value, 128);
-    }
+    /// <summary>
+    /// Max 128 bytes
+    /// </summary>
+    public string? details { get; init; }
 
-    public required string state
-    {
-        get => _state;
-        [MemberNotNull(nameof(_state))]
-        init => _state = GetBoundedString(value, 128);
-    }
+    /// <summary>
+    /// Max 128 bytes
+    /// </summary>
+    public string? state { get; init; }
 
     public Assets? assets { get; init; }
 
     public TimeStamps? timestamps { get; init; }
 
-    public Party? party { get; init; }
-
-    public Button[]? buttons
-    {
-        get => _buttons;
-        init => _buttons = value?.Length > 2
-            ? throw new ArgumentOutOfRangeException(nameof(buttons), value.Length, "Array can only hold up to two elements")
-            : value;
-    }
-
-    public bool instance { get; init; } = true;
-
-    public static string GetBoundedString(string value, int byteLength, [CallerMemberName] string? propertyName = null)
-    {
-        value = value.Trim();
-        if (Encoding.UTF8.GetByteCount(value) > byteLength)
-            throw new ArgumentOutOfRangeException(propertyName, value, $"Must be within {byteLength} bytes");
-        return value;
-    }
+    /// <summary>
+    /// Max 2 buttons
+    /// </summary>
+    public Button[]? buttons { get; init; }
 }
