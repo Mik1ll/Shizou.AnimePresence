4ff8dfde9ec157761b068758808f9c89e405a5c8
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-02-06 23:36:16 -0800

    Add vlc anime presence lua

---
+++ vlc_anime_presence.lua
@@ -0,0 +1,56 @@
+﻿local dkjson = require("dkjson")
+local httprequests = require("httprequests")
+
+local function callback_status(data, query)
+    return dkjson.encode(httprequests.getstatus(true))
+end
+
+local function sleep(sec)
+    vlc.misc.mwait(vlc.misc.mdate() + sec * 1000000)
+end
+
+
+local allow_restricted = "false"
+
+local discord_client_id_str = "1230418743734042694"
+
+sleep(5)
+
+math.randomseed(os.time())
+local oldhost = vlc.config.get("http-host")
+local host = "127.234.133.79"
+vlc.config.set("http-host", host)
+local port = math.random(1024, 65535)
+local oldport = vlc.config.get("http-port")
+vlc.config.set("http-port", port)
+
+vlc.msg.info("host:" .. host)
+vlc.msg.info("port:" .. port)
+
+local password = vlc.var.inherit(nil, "http-password") or "password"
+
+local h = vlc.httpd()
+local statush = h:file("/status.json", "application/json", nil, password, callback_status, nil)
+
+vlc.config.set("http-host", oldhost)
+vlc.config.set("http-port", oldport)
+
+local function find_in_datadir(subpath)
+    local list = vlc.config.datadir_list(subpath)
+    for _, l in ipairs(list) do
+        local s = vlc.net.stat(l)
+        if s then
+            return l
+        end
+    end
+    return nil
+end
+
+local subpath = "intf/Shizou.AnimePresence"
+local exePath = find_in_datadir(subpath) or find_in_datadir(subpath .. ".exe")
+if not exePath then
+    vlc.msg.err("Unable to find " .. subpath .. " in datadirs")
+    return
+end
+vlc.msg.info("exePath:" .. exePath)
+os.execute('"' .. exePath .. '"' .. discord_client_id_str .. ' ' .. allow_restricted .. ' vlc ' .. port)
