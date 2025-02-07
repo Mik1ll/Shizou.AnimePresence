ef5b9c61d874e22e80e0c7c1515e61d34ab710e8
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-02-06 22:24:44 -0800

    Use System.CommandLine

--- Program.cs
+++ Program.cs
@@ -1,31 +1,55 @@
-﻿using Shizou.AnimePresence;
+﻿using System.CommandLine;
+using Shizou.AnimePresence;
+using Command = System.CommandLine.Command;
 
+var rootCommand = new RootCommand()
+{
+    TreatUnmatchedTokensAsErrors = true,
+};
 
-if (args.Length != 3)
-    throw new InvalidOperationException("Must provide three arguments: discord client id, ipc socket/pipe name, and allow restricted (bool)");
+var discordClientIdArg = new Argument<string>("discord-client-id", "Discord API client ID");
 
-var discordClientId = args[0];
-var socketName = args[1];
-var allowRestricted = bool.Parse(args[2]);
+var allowRestrictedArg = new Argument<bool>("allow-restricted", "Whether to show presence for restricted series (hentai), will never show images");
 
-try
+var socketNameArg = new Argument<string>("socket-name", "The name of the ipc socket");
+
+var mpvCommand = new Command("mpv", "Run with mpv external player");
+
+rootCommand.AddArgument(discordClientIdArg);
+rootCommand.AddArgument(allowRestrictedArg);
+mpvCommand.AddArgument(socketNameArg);
+rootCommand.AddCommand(mpvCommand);
+mpvCommand.SetHandler(HandleMpv, discordClientIdArg, allowRestrictedArg, socketNameArg);
+
+await rootCommand.InvokeAsync(args);
+
+return;
+
+async Task HandleMpv(string discordClientId, bool allowRestricted, string socketName)
 {
     var cancelSource = new CancellationTokenSource();
     using var discordClient = new DiscordPipeClient(discordClientId, allowRestricted);
     using var mpvClient = new MpvPipeClient(socketName, discordClient);
     await mpvClient.Connect(cancelSource.Token);
     var tasks = new[] { mpvClient.ReadLoop(cancelSource.Token), mpvClient.QueryLoop(cancelSource.Token), discordClient.ReadLoop(cancelSource.Token) };
+    await Run(tasks, cancelSource);
+}
 
-    await Task.WhenAny(tasks);
-    await cancelSource.CancelAsync();
-    await Task.WhenAll(tasks);
-}
-catch (AggregateException ae)
+async Task Run(Task[] tasks, CancellationTokenSource cancelSource)
 {
-    ae.Handle(ex => ex is OperationCanceledException or IOException);
-}
-catch (Exception e) when (e is OperationCanceledException or IOException)
-{
+    try
+    {
+        await Task.WhenAny(tasks);
+        await cancelSource.CancelAsync();
+        await Task.WhenAll(tasks);
+    }
+    catch (AggregateException ae)
+    {
+        ae.Handle(ex => ex is OperationCanceledException or IOException);
+    }
+    catch (Exception e) when (e is OperationCanceledException or IOException)
+    {
+    }
 }
 
 public static partial class Program
