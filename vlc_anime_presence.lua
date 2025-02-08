267547afc196db0690a8c53c75d10bd2f283e682
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-02-08 00:13:42 -0800

    Update vlc lua intf

--- vlc_anime_presence.lua
+++ vlc_anime_presence.lua
@@ -1,13 +1,16 @@
 ﻿local dkjson = require("dkjson")
-local httprequests = require("httprequests")
 
 local function callback_status(data, query)
     local status = {}
-    local item = vlc.player.item()
+    local item = vlc.input.item() -- vlc 4.0 ~ vlc.player.item()
     if item then
-        status.uri = item.uri() 
+        status.uri = item:uri()
+        status.speed = vlc.var.get(vlc.object.input(), "rate") -- vlc 4.0 ~ vlc.player.get_rate()
+        status.paused = vlc.playlist.status() ~= "playing"
+        local duration = item:duration()
+        if duration > 0 then status.duration = duration end
+        status.time = vlc.var.get(vlc.object.input(), "time") / 1000000
     end
-    
     return dkjson.encode(status)
 end
 
@@ -34,7 +37,7 @@ vlc.msg.info("host:" .. host)
 vlc.msg.info("port:" .. port)
 
 local h = vlc.httpd()
-local statush = h:file("/status.json", "application/json", nil, "password", callback_status, nil)
+local statush = h:file("/status.json", "application/json", nil, "password", callback_status, vlc)
 
 vlc.config.set("http-host", oldhost)
 vlc.config.set("http-port", oldport)
